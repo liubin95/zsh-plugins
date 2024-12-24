f5b9d2bb3b6a251ad7cc746c3ce9e63b2e94e34b
liubin <liu_bin4835@outlook.com>
2024-12-24 14:27:34 +0800

    修改为docker clean all

--- docker-clean-all/docker-clean-all.plugin.zsh
+++ docker-clean-all/docker-clean-all.plugin.zsh
@@ -1,22 +1,22 @@
 #!/bin/zsh
 
-podman-clean-all() {
+docker-clean-all() {
   # 删除所有停止的容器
-  if [ "$(podman ps -a -q)" ]; then
-    podman rm $(podman ps -a -q)
+  if [ "$(docker ps -a -q)" ]; then
+    docker rm $(docker ps -a -q)
   else
     echo "No containers to remove."
   fi
 
   # 清理未使用的卷
-  podman volume prune -f
+  docker volume prune -f
 
   # 清理未使用的镜像
-  podman image prune -a -f
+  docker image prune -a -f
 
   # 清理未使用的系统资源，包括网络和卷
-  podman system prune --volumes -f
+  docker system prune --volumes -f
 
   # 清理构建缓存
-  podman builder prune -a -f
+  docker builder prune -a -f
 }
