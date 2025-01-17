bfe4103fa2aab0c893abc5ae602b578716217460
liubin <liu_bin4835@outlook.com>
2025-01-17 12:27:02 +0800

    迁移到wezterm

--- tt/tt.plugin.zsh
+++ tt/tt.plugin.zsh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
-tt(){
+tt() {
   readonly name=${1:?"The name must be specified."}
-  printf "\e]1337;SetBadgeFormat=%s\a" "$(echo "$name" | base64)"
+  wezterm cli set-tab-title "$name"
 }
 
-tc(){
+tc() {
   readonly color=${1:?"The color must be specified."}
   local valids=("red" "green" "yellow" "clean")
   # shellcheck disable=SC2199
@@ -15,29 +15,29 @@ tc(){
   fi
   case "$color" in
     red)
-        printf "\033]6;1;bg;red;brightness;255\a"
-        printf "\033]6;1;bg;green;brightness;85\a"
-        printf "\033]6;1;bg;blue;brightness;85\a"
-        ;;
+      printf "\033]6;1;bg;red;brightness;255\a"
+      printf "\033]6;1;bg;green;brightness;85\a"
+      printf "\033]6;1;bg;blue;brightness;85\a"
+      ;;
     green)
-        printf "\033]6;1;bg;red;brightness;80\a"
-        printf "\033]6;1;bg;green;brightness;250\a"
-        printf "\033]6;1;bg;blue;brightness;113\a"
-        ;;
+      printf "\033]6;1;bg;red;brightness;80\a"
+      printf "\033]6;1;bg;green;brightness;250\a"
+      printf "\033]6;1;bg;blue;brightness;113\a"
+      ;;
     yellow)
-        printf "\033]6;1;bg;red;brightness;241\a"
-        printf "\033]6;1;bg;green;brightness;250\a"
-        printf "\033]6;1;bg;blue;brightness;140\a"
-        ;;
+      printf "\033]6;1;bg;red;brightness;241\a"
+      printf "\033]6;1;bg;green;brightness;250\a"
+      printf "\033]6;1;bg;blue;brightness;140\a"
+      ;;
     clean)
-        printf "\033]6;1;bg;*;default\a"
-        ;;
+      printf "\033]6;1;bg;*;default\a"
+      ;;
   esac
 }
 
 # sleep 5 && tmsg
-tmsg(){
-  # shellcheck disable=SC2154
-  printf "\033]9;[%s]\a" "${history[$((HISTCMD-2))]} finished with $?"
+tmsg() {
+  local previous_command=$(fc -ln -1)
+  local exit_status=$?
+  printf "\e]9;%s\e\\" "$previous_command finished with $exit_status"
 }
-
