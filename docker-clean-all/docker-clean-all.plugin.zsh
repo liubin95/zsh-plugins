1579690bf60c01e275b3b83332d9b4bbe548bc2e
liubin <liu_bin4835@outlook.com>
2025-02-06 15:44:43 +0800

    docker clean fix

--- docker-clean-all/docker-clean-all.plugin.zsh
+++ docker-clean-all/docker-clean-all.plugin.zsh
@@ -1,22 +1,31 @@
 #!/bin/zsh
 
 docker-clean-all() {
+  # 判断时docker还是podman
+  if command -v docker &>/dev/null; then
+    DOCKER=docker
+  elif command -v podman &>/dev/null; then
+    DOCKER=podman
+  else
+    echo "No docker or podman installed."
+    return 1
+  fi
   # 删除所有停止的容器
   if [ "$(docker ps -a -q)" ]; then
-    docker rm $(docker ps -a -q)
+    $DOCKER rm $($DOCKER ps -a -q)
   else
     echo "No containers to remove."
   fi
 
   # 清理未使用的卷
-  docker volume prune -f
+  $DOCKER volume prune -f
 
   # 清理未使用的镜像
-  docker image prune -a -f
+  $DOCKER image prune -a -f
 
   # 清理未使用的系统资源，包括网络和卷
-  docker system prune --volumes -f
+  $DOCKER system prune --volumes -f
 
   # 清理构建缓存
-  docker builder prune -a -f
+  $DOCKER builder prune -a -f
 }
