d9b92e3f78a62e3c952d4d8e8ef825a7f45c4dbd
liubin <liu_bin4835@outlook.com>
2025-02-07 14:23:14 +0800

    ksw 环境从KSW_ENVIRONMENTS

--- ksw/ksw.plugin.zsh
+++ ksw/ksw.plugin.zsh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
 ksw(){
-  local valid_envs=("dev" "prd" "minikube" "heytea" "banu")
+
+    local valid_envs=(${(s: :)KSW_ENVIRONMENTS})
   readonly env=${1:?"The env must be specified."}
   if [[ ! "${valid_envs[@]}" =~ "${env}" ]]; then
     echo "错误: 无效的环境 $1. 有效选项为: ${valid_envs[*]}"
