91eb22f2e998a1e66fa90d8719a089c650befbfa
liubin <liu_bin4835@outlook.com>
2025-04-21 13:51:03 +0800

    update tt

--- ksw/ksw.plugin.zsh
+++ ksw/ksw.plugin.zsh
@@ -1,8 +1,7 @@
 #!/bin/zsh
 
 ksw(){
-
-    local valid_envs=(${(s: :)KSW_ENVIRONMENTS})
+  local valid_envs=(${(s: :)KSW_ENVIRONMENTS})
   readonly env=${1:?"The env must be specified."}
   if [[ ! "${valid_envs[@]}" =~ "${env}" ]]; then
     echo "错误: 无效的环境 $1. 有效选项为: ${valid_envs[*]}"
@@ -17,12 +16,16 @@ ksw(){
         tc red
         tt heytea@k8s
         ;;
+    singapore)
+        tc red
+        tt singapore@k8s
+        ;;
     dev)
         tc green
         tt dev@k8s
         ;;
     minikube)
-        tc yellow
+        tc green
         tt minikube@k8s
         ;;
   esac
