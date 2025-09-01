8c3e1d19a04b202830aeed7303ec4949094f512d
bin.liu <liu_bin4835@outlook.com>
2025-09-01 14:30:27 +0800

    fix tt;使用ECMA-48 转义

--- ksw/ksw.plugin.zsh
+++ ksw/ksw.plugin.zsh
@@ -1,33 +1,19 @@
 #!/bin/zsh
 
-ksw(){
+ksw() {
   local valid_envs=(${(s: :)KSW_ENVIRONMENTS})
   readonly env=${1:?"The env must be specified."}
   if [[ ! "${valid_envs[@]}" =~ "${env}" ]]; then
     echo "错误: 无效的环境 $1. 有效选项为: ${valid_envs[*]}"
     return 1
   fi
-  case "$env" in
-    prd)
-        tc red
-        tt prd@k8s
-        ;;
-    heytea)
-        tc red
-        tt heytea@k8s
-        ;;
-    singapore)
-        tc red
-        tt singapore@k8s
-        ;;
-    dev)
-        tc green
-        tt dev@k8s
-        ;;
-    minikube)
-        tc green
-        tt minikube@k8s
-        ;;
-  esac
+
+  local prd_envs=(${(s: :)KSW_ENVIRONMENTS_PRD})
+  if [[ "${prd_envs[@]}" =~ "${env}" ]]; then
+    tt "🔴 ${env}@k8s"
+  else
+    tt "🟢 ${env}@k8s"
+  fi
+
   export KUBECONFIG=~/.kube/admin.conf-"${env}".yaml
 }
--- tt/tt.plugin.zsh
+++ tt/tt.plugin.zsh
@@ -2,22 +2,5 @@
 
 tt() {
   readonly name=${1:?"The name must be specified."}
-  wezterm cli set-tab-title "$name"
-}
-
-tc() {
-  readonly color=${1:?"The color must be specified."}
-  local valids=("red" "green" "yellow" "clean")
-  # shellcheck disable=SC2199
-  if [[ ! "${valids[@]}" =~ "${color}" ]]; then
-    echo "错误: 无效的环境 $1. 有效选项为: ${valids[*]}"
-    return 1
-  fi
-  printf "\033]1337;SetUserVar=tab_color=%s\007" $(echo -n "$color" | base64)
-
-}
-
-# sleep 5 && tmsg
-tmsg() {
-  printf "\033]9;%s\007" "job finished"
+  echo -e "\e]0;$name\a"
 }
